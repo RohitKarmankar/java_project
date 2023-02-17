35e5a3be07f09c687df509c139ccbde0bcc50a58
JOHN <JOHN@DESKTOP-6AG2VUU>
2023-02-17 02:04:59 -0800

    non static varible JIRA[1010]

---
+++ java_project/src/Loops/Base_Class.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class Base_Class {
+    int a=100;
+    int b=100;
+	public static void main(String[] args) {
+		Base_Class kk=new Base_Class();
+		int c=kk.a+kk.b;
+		System.out.println(c);
+	}
+}
