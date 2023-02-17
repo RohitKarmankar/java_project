e6fc2f3dccd2821d457e95cacc6adeb482f64f91
rohitkarmankar327@gmail.com <rohitkarmankar327@gmail.com>
2023-02-17 01:40:26 -0800

    message for commit in java_project JIRA[456]

---
+++ java_project/src/Loops/Evennumber_using_do_while_loop.java
@@ -0,0 +1,18 @@
+package Loops;
+
+public class Evennumber_using_do_while_loop {
+
+	public static void main(String[] args) {
+	int x=100;
+	do {
+	if(x%2==0) {
+	System.out.print(x+" ");	
+	}
+	x--;
+	}
+	while(x<=1);
+	}
+	
+	
+	
+}
